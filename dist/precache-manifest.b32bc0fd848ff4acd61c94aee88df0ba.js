5a983bf06ae921de2520ae7a283abdf1f4f20477
lihongxun945 <lihongxun945@gmail.com>
2018-07-18 20:25:24 +0800

    build

---
+++ dist/precache-manifest.b32bc0fd848ff4acd61c94aee88df0ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e24e88e0468f54e8e2e3d3e8d72ad3fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/img/sad.gif"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/img/sad.ffff4377.gif"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/img/haha.gif"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/img/haha.9ad347b5.gif"
+  },
+  {
+    "revision": "5442f90f0a7d84ccc30d008200e3aee6",
+    "url": "/img/board.5442f90f.jpg"
+  },
+  {
+    "revision": "86aebbe6401945463f1c",
+    "url": "/css/app.9670d04d.css"
+  },
+  {
+    "revision": "86aebbe6401945463f1c",
+    "url": "/app.bundle.js"
+  },
+  {
+    "revision": "b0e9403d2b633b0bf38d",
+    "url": "/ai.bundle.js"
+  }
+];
---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.b32bc0fd848ff4acd61c94aee88df0ba.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "gobang-v2"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
